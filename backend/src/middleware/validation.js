1e3abd64cf109aa34a8ffbbe722ec5ffa180e37a
YEMURAI-CAROLINE-MLAMBO <95072287+YEMURAI-CAROLINE-MLAMBO@users.noreply.github.com>
2025-08-14 15:33:37 +0200

    Create validation.js

---
+++ backend/src/middleware/validation.js
@@ -0,0 +1,22 @@
+const { validationResult } = require('express-validator');
+
+/**
+ * Validate request middleware
+ */
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ 
+      errors: errors.array().map(err => ({
+        param: err.param,
+        message: err.msg,
+        location: err.location
+      }))
+    });
+  }
+  next();
+};
+
+module.exports = {
+  validate
+};
