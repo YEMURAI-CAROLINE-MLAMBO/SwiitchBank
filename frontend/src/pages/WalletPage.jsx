adf8560f0ff760cf87a101c7712aeb27b9320952
YEMURAI-CAROLINE-MLAMBO <95072287+YEMURAI-CAROLINE-MLAMBO@users.noreply.github.com>
2025-08-20 10:12:15 +0200

    Create WalletPage.jsx

---
+++ frontend/src/pages/WalletPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WalletDashboard from '../components/wallet/WalletDashboard';
+
+function WalletPage() {
+  return (
+    <div className="wallet-page">
+      <h1>My Wallet</h1>
+      <WalletDashboard />
+    </div>
+  );
+}
+
+export default WalletPage;
