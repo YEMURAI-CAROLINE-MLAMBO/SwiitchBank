bd288c9d3b8e8edce6dd025b50374a4902d4809a
YEMURAI-CAROLINE-MLAMBO <95072287+YEMURAI-CAROLINE-MLAMBO@users.noreply.github.com>
2025-08-20 09:55:51 +0200

    Create App.jsx
    /

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AppLayout from './layouts/AppLayout';
+import HomePage from './pages/HomePage';
+import WalletPage from './pages/WalletPage';
+import CardsPage from './pages/CardsPage';
+// Import other pages and components as needed
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="wallet" element={<WalletPage />} />
+          <Route path="cards" element={<CardsPage />} />
+          {/* Add other routes here */}
+        </Route>
+        {/* Add routes for login, signup, etc. outside the layout if needed */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
