a08febb7683ef40cadcf4c238b2a252b6d606738
ymlambo21 <ymlambo21@gmail.com>
2025-09-02 06:10:12 +0000

    Changes before Firebase Studio auto-run

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      {/* Placeholder for login form */}
+    </div>
+  );
+};
+
+export default LoginPage;
